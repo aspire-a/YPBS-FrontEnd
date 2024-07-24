149fbd6f70b82aa67be0528fc74b69ea862f1c29
Enes BEKTAŞ <enes.bektas@tubitak.gov.tr>
2024-07-24 09:43:59 +0300

    YPBS254: Bugün doğanlar için MUI style eklendi

--- ypbs-fe/app/api_helper/URLs.tsx
+++ ypbs-fe/app/api_helper/URLs.tsx
@@ -1,4 +1,6 @@
 // Update this
 export const COMMON_PATH = "localhost:8080/";
 
-export const LOGIN_PATH = () => "login";
+export const LOGIN_PATH = (username, password) => "api/users/login" + "?username=" + username + "&password=" + password;
+
+export const BORN_TODAY_PATH = () => "api/users/born-today";
--- ypbs-fe/app/born_today/page.tsx
+++ ypbs-fe/app/born_today/page.tsx
@@ -4,11 +4,16 @@ import { getFetcher } from "../api_helper/fetchers";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { BornTodayCard } from "./born_today_card";
+import { createTheme } from "@/lib/theme/theme";
+import { ThemeProvider } from "@mui/material/styles";
+import { Box } from "@mui/material";
+import { BORN_TODAY_PATH } from "../api_helper/URLs";
+
 
 export default function Page() {
   const [users, setUsers] = useState([]);
 
-  const { data, error } = useSWR('/api/users/born-today', getFetcher);
+  const { data, error } = useSWR(BORN_TODAY_PATH, getFetcher);
 
   if(error){
     return <div>Not able to load User.</div>
@@ -16,14 +21,21 @@ export default function Page() {
 
   useEffect(() => {
     if(data){
-      console.log(data)
       setUsers(data);
     }
   }, [data]);
 
+  const theme = createTheme();
+
+
   return (
     <div >
-      {users.map((user) =>  (<BornTodayCard key={user.name + " " + user.surname} name={user.name} surname={user.surname} birthdate={user.birthDate} image={user.image}/>))}
+      <Box>
+        {users.map((user) =>  (<BornTodayCard key={user.name + " " + user.surname} name={user.name} surname={user.surname} birthdate={user.birthDate} image={user.image}/>))}
+
+      </Box>
+      <ThemeProvider theme={theme}> 
+      </ThemeProvider>
     </div>
   );
 }
